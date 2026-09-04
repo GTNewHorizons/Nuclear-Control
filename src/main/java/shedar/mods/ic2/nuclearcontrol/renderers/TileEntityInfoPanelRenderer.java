1f0e0e2fd0eafa9bb6891670d0cc63fc063fbc8d
boubou19 <miisterunknown@gmail.com>
2026-09-04 03:32:33 +0200

    offset panel text from face

--- src/main/java/shedar/mods/ic2/nuclearcontrol/renderers/TileEntityInfoPanelRenderer.java
+++ src/main/java/shedar/mods/ic2/nuclearcontrol/renderers/TileEntityInfoPanelRenderer.java
@@ -202,7 +202,9 @@ public class TileEntityInfoPanelRenderer extends TileEntitySpecialRenderer {
             if (panel instanceof TileEntityAdvancedInfoPanel && screen != null) {
                 TileEntityAdvancedInfoPanel advPanel = (TileEntityAdvancedInfoPanel) panel;
                 deltas = MODEL.getDeltas(advPanel, screen, deltasBuffer);
-                thickness = (float) (advPanel.thickness / 16F - (deltas[0] + deltas[1] + deltas[2] + deltas[3]) / 4);
+                float surfaceDepth = advPanel.thickness == 16 ? 0.98F : Math.max(advPanel.thickness, 1) / 16F;
+                thickness = (float) (surfaceDepth + 0.02F
+                        - (deltas[0] + deltas[1] + deltas[2] + deltas[3]) / 4);
             }
 
             GL11.glTranslatef(dx + displayWidth / 2, thickness, dz + displayHeight / 2);
